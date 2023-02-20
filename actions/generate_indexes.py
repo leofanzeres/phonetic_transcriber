ec931da208d5dcd7395a4240beee1e9a58a250b5
Leonardo <lfanzeres@verbio.com>
2023-02-20 01:18:56 +0100

    Removed the file from the git repository.

--- actions/generate_indexes.py
+++
@@ -1,7 +0,0 @@
-import utils as ut
-import values as v
-from language import Language
-
-dictionary = ut.load_dictionary(v.PT_BR_DICTIONARY_FILE,'\t')
-language = Language(v.PHONEMES_FILE, v.LETTERS_FILE)
-language.generate_indexes(dictionary)
